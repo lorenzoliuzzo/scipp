8e0af41dfa3f71e8c2ae1a73eb23dac179651324
lorenzoliuzzo <lorenzo.liuzzo1@studenti.unimi.it>
2023-01-12 21:43:12 +0100

    top scipp

---
+++ sci++.hpp
@@ -0,0 +1,28 @@
+/**
+ * @file    sci++.hpp
+ * @author  Lorenzo Liuzzo (lorenzoliuzzo@outlook.com)
+ * @brief   
+ * @date    2023-01-12
+ * 
+ * @copyright Copyright (c) 2023
+ */
+
+
+#ifndef SCIPP_LIBRARY
+
+
+    #define SCIPP_LIBRARY
+
+
+        #ifndef SCIPP_MEASUREMENTS_HPP
+
+
+            #include "measurements/measurements.hpp"
+
+                using namespace measurements;
+
+
+        #endif // SCIPP_MEASUREMENTS_HPP
+
+
+#endif

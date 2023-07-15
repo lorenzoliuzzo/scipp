d0828f6a6b448d287a07acb04f6d1ac380a6d998
lorenzoliuzzo <lorenzo.liuzzo1@studenti.unimi.it>
2023-07-15 15:42:37 +0200

    GG

--- trash/curves/sphere.hpp
+++
@@ -1,8 +0,0 @@
-/**
- * @file    math/calculus/curves/sphere.hpp
- * @author  Lorenzo Liuzzo (lorenzoliuzzo@outlook.com)
- * @brief   This file contains the implementation 
- * @date    2023-06-09
- * 
- * @copyright Copyright (c) 2023
- */
